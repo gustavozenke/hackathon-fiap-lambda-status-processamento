b79bde474ed4fb82f2a23ef37b5d92ae46d849ea
Gustavo Henrique Zenke <gustavozenke01@gmail.com>
2025-02-01 09:40:19 -0300

    ajuste lambda handler

--- infra/variables.tf
+++ infra/variables.tf
@@ -10,7 +10,7 @@ variable "function_role" {
 
 variable "handler" {
   type        = string
-  default     = "main.lambda_handler"
+  default     = "application.router.lambda_handler.lambda_handler"
 }
 
 variable "runtime" {
