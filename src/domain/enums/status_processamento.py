d06700f375b14d5e45fac5ec114a6badbade978d
Gustavo Henrique Zenke <gustavozenke01@gmail.com>
2025-02-01 10:42:16 -0300

    Enum status de processamento

---
+++ src/domain/enums/status_processamento.py
@@ -0,0 +1,15 @@
+from enum import Enum
+
+
+class StatusProcessamento(Enum):
+    PROCESSAMENTO_NAO_INICIADO = "Processamento não iniciado"
+    PROCESSAMENTO_EM_ANDAMENTO = "Processamento em andamento"
+    PROCESSAMENTO_FINALIZADO_SUCESSO = "Processamento finalizado com sucesso"
+    PROCESSAMENTO_FINALIZADO_ERRO = "Processamento finalizado com erro"
+
+    @staticmethod
+    def converter_para_enum(valor: str):
+        for status in StatusProcessamento:
+            if status.name == valor:
+                return status
+        raise ValueError(f"Status processmento invalido: '{valor}'")
