f6e3213bea16bc60df5d8c80f4cec2fb44a43fa5
Gustavo Henrique Zenke <gustavozenke01@gmail.com>
2025-02-02 08:33:05 -0300

    Separacao entre controller gateway e integracao sqs

--- src/application/router/lambda_handler.py
+++ src/application/router/lambda_handler.py
@@ -9,113 +9,32 @@ video_service = VideoService(video_repository)
 
 
 def lambda_handler(event, context):
-    try:
-        http_method = event['httpMethod']
-        path = event['path'].split('/')[1]
-        nome_usuario = event['path'].split('/')[2]
-
-        if http_method == 'GET' and path == 'status-processamento':
-            result = video_service.consultar_status_video(nome_usuario)
-            return {'statusCode': 200, 'body': json.dumps(result)}
-
-        elif http_method == 'POST' and path == 'status-processamento':
-            status = event['body']
-            nome_video = event['body']
-            status_processamento = StatusProcessamento.converter_para_enum(status['status_processamento'])
-            result = video_service.criar_status_video(nome_usuario, status_processamento, nome_video)
-            return {'statusCode': 200, 'body': json.dumps(result)}
-
-        return {'statusCode': 404, 'body': json.dumps({'message': 'Route not found'})}
-    except ValueError as error:
-        return {'statusCode': 400, 'body': json.dumps({'message': f'Evento invalido. Erro: {error.args}'})}
+    if event['Records']:
+        sqs_controller(event)
+    elif event['resource']:
+        gateway_controller(event)
 
 
-if __name__ == '__main__':
-    event = {
-        "resource": "/status-processamento/{nome_usuario}",
-        "path": "/status-processamento/gustavozenke",
-        "httpMethod": "GET",
-        "headers": {
-            "Accept": "*/*",
-            "Accept-Encoding": "gzip, deflate, br",
-            "Authorization": "Bearer eyJraWQiOiJ0aUFpT2RzQ2ZWc0lST1RKRU8yZVdpMmdiM1FrdFBOOFhzRG5lenJtR0ljPSIsImFsZyI6IlJTMjU2In0.eyJzdWIiOiIzNGE4NjRmOC0zMDUxLTcwMzItMjU0Mi04OTRmZjkyOGUzY2QiLCJpc3MiOiJodHRwczpcL1wvY29nbml0by1pZHAudXMtZWFzdC0xLmFtYXpvbmF3cy5jb21cL3VzLWVhc3QtMV9TdU00dmRiUVMiLCJ2ZXJzaW9uIjoyLCJjbGllbnRfaWQiOiI2NTM5MmozajlsamsydG5zdWdiYTljaWhkYiIsIm9yaWdpbl9qdGkiOiIwZDhjMTZmZS1iNWFiLTQ5ZmQtYmY2My1mOGMxMTJlZDY4ZGYiLCJ0b2tlbl91c2UiOiJhY2Nlc3MiLCJzY29wZSI6Im9wZW5pZCBwcm9maWxlIGVtYWlsIiwiYXV0aF90aW1lIjoxNzM4NDU5NzIwLCJleHAiOjE3Mzg0NjMzMjAsImlhdCI6MTczODQ1OTcyMSwianRpIjoiYjk5ZjJlNjItMDZjYy00Njg0LWE0YmUtMWNkMDE2ZjhiOGNjIiwidXNlcm5hbWUiOiJndXN0YXZvemVua2UifQ.GTJXIPPOtm7aev5dTcNbd-cg2YNnW7GnIrGjL3OEPimam2y0hFKlUPa4yg1y6G_ZJkXAc1KoSeCnCP5u4d-pXtzhpy5r0cXF2XkKlmfjgcMbIHARc3KTcTufrQUUkic8TiNk48B3QaJ4VXmN1OqAwla_sJLzV_KrkG_vVel0dadCA3PyC9A6toBK2miOgOJJgG_Zluaxte14svgq0177tNHKfmY6_yKKqtSLZbG9A06qj2_P1NFBwMOXwpzt-VWueIuidVqcC8TA-4DAEj4x0j_i6qUQeQYI1zq31vuZqsz7t677tAlvCbtVmh9-ft4QG5CVbpggW72dNiZpp7zMdg",
-            "Cache-Control": "no-cache",
-            "Host": "ahgifxc4a1.execute-api.us-east-1.amazonaws.com",
-            "Postman-Token": "1def5a01-9717-4a19-919e-4eec61f760d8",
-            "User-Agent": "PostmanRuntime/7.26.8",
-            "X-Amzn-Trace-Id": "Root=1-679ecc36-048044ba17056bcb2be94f46",
-            "X-Forwarded-For": "187.10.135.221",
-            "X-Forwarded-Port": "443",
-            "X-Forwarded-Proto": "https",
-        },
-        "multiValueHeaders": {
-            "Accept": ["*/*"],
-            "Accept-Encoding": ["gzip, deflate, br"],
-            "Authorization": [
-                "Bearer eyJraWQiOiJ0aUFpT2RzQ2ZWc0lST1RKRU8yZVdpMmdiM1FrdFBOOFhzRG5lenJtR0ljPSIsImFsZyI6IlJTMjU2In0.eyJzdWIiOiIzNGE4NjRmOC0zMDUxLTcwMzItMjU0Mi04OTRmZjkyOGUzY2QiLCJpc3MiOiJodHRwczpcL1wvY29nbml0by1pZHAudXMtZWFzdC0xLmFtYXpvbmF3cy5jb21cL3VzLWVhc3QtMV9TdU00dmRiUVMiLCJ2ZXJzaW9uIjoyLCJjbGllbnRfaWQiOiI2NTM5MmozajlsamsydG5zdWdiYTljaWhkYiIsIm9yaWdpbl9qdGkiOiIwZDhjMTZmZS1iNWFiLTQ5ZmQtYmY2My1mOGMxMTJlZDY4ZGYiLCJ0b2tlbl91c2UiOiJhY2Nlc3MiLCJzY29wZSI6Im9wZW5pZCBwcm9maWxlIGVtYWlsIiwiYXV0aF90aW1lIjoxNzM4NDU5NzIwLCJleHAiOjE3Mzg0NjMzMjAsImlhdCI6MTczODQ1OTcyMSwianRpIjoiYjk5ZjJlNjItMDZjYy00Njg0LWE0YmUtMWNkMDE2ZjhiOGNjIiwidXNlcm5hbWUiOiJndXN0YXZvemVua2UifQ.GTJXIPPOtm7aev5dTcNbd-cg2YNnW7GnIrGjL3OEPimam2y0hFKlUPa4yg1y6G_ZJkXAc1KoSeCnCP5u4d-pXtzhpy5r0cXF2XkKlmfjgcMbIHARc3KTcTufrQUUkic8TiNk48B3QaJ4VXmN1OqAwla_sJLzV_KrkG_vVel0dadCA3PyC9A6toBK2miOgOJJgG_Zluaxte14svgq0177tNHKfmY6_yKKqtSLZbG9A06qj2_P1NFBwMOXwpzt-VWueIuidVqcC8TA-4DAEj4x0j_i6qUQeQYI1zq31vuZqsz7t677tAlvCbtVmh9-ft4QG5CVbpggW72dNiZpp7zMdg"
-            ],
-            "Cache-Control": ["no-cache"],
-            "Host": ["ahgifxc4a1.execute-api.us-east-1.amazonaws.com"],
-            "Postman-Token": ["1def5a01-9717-4a19-919e-4eec61f760d8"],
-            "User-Agent": ["PostmanRuntime/7.26.8"],
-            "X-Amzn-Trace-Id": ["Root=1-679ecc36-048044ba17056bcb2be94f46"],
-            "X-Forwarded-For": ["187.10.135.221"],
-            "X-Forwarded-Port": ["443"],
-            "X-Forwarded-Proto": ["https"],
-        },
-        "queryStringParameters": None,
-        "multiValueQueryStringParameters": None,
-        "pathParameters": {"nome_usuario": "gustavozenke"},
-        "stageVariables": None,
-        "requestContext": {
-            "resourceId": "xnjtk0",
-            "authorizer": {
-                "claims": {
-                    "origin_jti": "0d8c16fe-b5ab-49fd-bf63-f8c112ed68df",
-                    "sub": "34a864f8-3051-7032-2542-894ff928e3cd",
-                    "token_use": "access",
-                    "scope": "openid profile email",
-                    "auth_time": "1738459720",
-                    "iss": "https://cognito-idp.us-east-1.amazonaws.com/us-east-1_SuM4vdbQS",
-                    "exp": "Sun Feb 02 02:28:40 UTC 2025",
-                    "version": "2",
-                    "iat": "Sun Feb 02 01:28:41 UTC 2025",
-                    "client_id": "65392j3j9ljk2tnsugba9cihdb",
-                    "jti": "b99f2e62-06cc-4684-a4be-1cd016f8b8cc",
-                    "username": "gustavozenke",
-                }
-            },
-            "resourcePath": "/status-processamento/{nome_usuario}",
-            "httpMethod": "GET",
-            "extendedRequestId": "FVTYmHqwIAMEH7Q=",
-            "requestTime": "02/Feb/2025:01:36:54 +0000",
-            "path": "/prod/status-processamento/gustavozenke",
-            "accountId": "369780787289",
-            "protocol": "HTTP/1.1",
-            "stage": "prod",
-            "domainPrefix": "ahgifxc4a1",
-            "requestTimeEpoch": 1738460214659,
-            "requestId": "56afcbed-cfcd-473f-83f0-c0515922d1c0",
-            "identity": {
-                "cognitoIdentityPoolId": None,
-                "accountId": None,
-                "cognitoIdentityId": None,
-                "caller": None,
-                "sourceIp": "187.10.135.221",
-                "principalOrgId": None,
-                "accessKey": None,
-                "cognitoAuthenticationType": None,
-                "cognitoAuthenticationProvider": None,
-                "userArn": None,
-                "userAgent": "PostmanRuntime/7.26.8",
-                "user": None,
-            },
-            "domainName": "ahgifxc4a1.execute-api.us-east-1.amazonaws.com",
-            "deploymentId": "lpeg8p",
-            "apiId": "ahgifxc4a1",
-        },
-        "body": None,
-        "isBase64Encoded": False,
-    }
+def gateway_controller(event):
+    http_method = event['httpMethod']
+    path = event['path'].split('/')[1]
+    nome_usuario = event['path'].split('/')[2]
 
-    lambda_handler(event, None)
+    if http_method == 'GET' and path == 'status-processamento':
+        result = video_service.consultar_status_video(nome_usuario)
+        return {'statusCode': 200, 'body': json.dumps(result)}
+
+    return {'statusCode': 404, 'body': json.dumps({'message': 'Route not found'})}
+
+
+def sqs_controller(event):
+    body = json.loads(event['Records'][0]['body'])
+    nome_usuario = body['nome_usuario']
+    nome_video = body['nome_video']
+    status_processamento = StatusProcessamento.converter_para_enum(body['status_processamento'])
+    result = video_service.criar_status_video(nome_usuario, status_processamento, nome_video)
+    return {'statusCode': 200, 'body': json.dumps(result)}
+
+
+# if __name__ == '__main__':
+#     lambda_handler(event, None)
